11f98b9b9e6bd8d82361b08b185ab8808ea00b86
egalland <emmanuel.galland117@gmail.com>
2018-06-22 15:12:15 +0200

    Exercice 1

---
+++ assets/javascript/script.js
@@ -0,0 +1,4 @@
+// FONCTION QUI AJOUTE UNE COULEUR SI ON PASSE LA SOURIS DESSUS, ET S'ENLEVE QUAND LA SOURIS N'EST PLUS DESSUS
+function borderColor(id, style){
+  document.getElementById(id).style = style;
+}
